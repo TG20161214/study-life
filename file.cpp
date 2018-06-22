f07588eda787c5e5e8e57cafc136dae42561d8d0
TG20161214 <40415597+TG20161214@users.noreply.github.com>
2018-06-22 16:09:37 +0800

    6.22
    完成修改模板
    完成保存模板
    完成添加模块
    完善姓名查询模块

--- file.cpp
+++ file.cpp
@@ -44,7 +44,7 @@ int read(ZGGZ t[])																					//文件读取模块
 {
 	   int i;
        FILE *fp;//定义文件指针
-       if ((fp = fopen("gz.dat", "rb")) == NULL)
+       if ((fp = fopen("gx.dat", "rb")) == NULL)
        {
               printf("工资数据文件打开失败，请检查文件是否存在或损坏后重试!");
               return 0;
@@ -136,14 +136,21 @@ int find(ZGGZ S[])																					 //工资查询模块
 			}
 			if (flag)
 			{
-				printf("\t\t工号:%s\n",S[i].id);  
-				printf("\t\t岗位工资:%f\n",S[i].money1);  
-				printf("\t\t薪级工资:%f\n",S[i].money2);  
-				printf("\t\t职务津贴:%f\n",S[i].money3);  
-				printf("\t\t绩效工资:%f\n",S[i].money4);  
-				printf("\t\t应发工资:%f\n",S[i].money);
-				printf("\t\t个人所得税:%f\n",S[i].tax);  
-				printf("\t\t实发工资:%f\n",S[i].wages);  
+					for (i = 0; i < n; i++)			//防止出现重名状况
+					{
+						if (strcmp(S[i].name,nm)==0)
+						{
+							printf("\n\t\t工号:%s\n",S[i].id);  
+							printf("\t\t岗位工资:%f\n",S[i].money1);  
+							printf("\t\t薪级工资:%f\n",S[i].money2);  
+							printf("\t\t职务津贴:%f\n",S[i].money3);  
+							printf("\t\t绩效工资:%f\n",S[i].money4);  
+							printf("\t\t应发工资:%f\n",S[i].money);
+							printf("\t\t个人所得税:%f\n",S[i].tax);  
+							printf("\t\t实发工资:%f\n",S[i].wages);  
+						}
+					}
+				
 			}
 			else
 			{
@@ -250,7 +257,7 @@ int list(ZGGZ S[])																											//浏览模块
 				scanf("\t\t%c",&flag); 
 			}
 		}
-		printf("\n工资数据已全部浏览完毕,即将自动返回主菜单\n\n");
+		printf("\n工资数据已浏览完毕,即将自动返回主菜单\n\n");
 	}
 	
 	if(m==2)																												//全部浏览
@@ -265,13 +272,219 @@ int list(ZGGZ S[])																											//浏览模块
 			printf("\n");
 			i++;		 
 		}
-		printf("\n工资数据已全部浏览完毕,即将自动返回主菜单\n\n");
+		printf("\n工资数据已浏览完毕,即将自动返回主菜单\n\n");
 	}
 	
 	if(m==3)																												//返回
 		{
 			return 0;
 		}
+	return 0;
+}
+
+int modify(ZGGZ S[])                                                                                                        //修改模块
+{
+	int m,j;  
+    char flag='y';  
+    while(flag!='n'&&flag!='N')  
+    {  
+		system("cls");
+        printf("\n\t\t***********修改方式************\n");  
+        printf("\t\t┌------------------------------┐\n");  
+        printf("\t\t│          1.检索工号          │\n");  
+        printf("\t\t│          2.检索姓名          │\n");  
+        printf("\t\t│          3.返回              │\n");    
+        printf("\t\t└------------------------------┘\n");  
+        printf("\n\t\t请选择查询方式:");  
+        scanf("\t\t%d",&m);					//选择修改方式  
+	
+		do										//检查输入是否符合规范  
+			{  
+				j=0;  
+				if(m!=1&&m!=2&&m!=3)  
+				{     
+					j=1;  
+					printf("\t\t您输入的检索方式不存在,请重新输入:");  
+					scanf("\t\t%d",&m);			//不符合规范则重新选择查询方式  
+				}  
+			}while(j); 
+
+		if(m==1)								//工号查询
+			{
+				int i;							//现在查询员工序号
+				int flag=0;						//标记员工有没有存在
+				char gonghao[10];
+				printf("\n");
+				printf("请输入要修改数据员工的编号：");
+				scanf("%s",gonghao);
+				for (i = 0; i < n; i++)
+				{
+					if (strcmp(S[i].id,gonghao)==0)
+					{
+						flag = 1;
+						break;
+					}
+				}
+				if (flag)
+				{	
+					printf("原始数据如下：");
+					printf("工号:%s  %s\n",S[i].id,S[i].name);  
+					printf("岗位工资:%f\t薪级工资:%f\t职务津贴:%f\t绩效工资:%f\n",S[i].money1,S[i].money2,S[i].money3,S[i].money4);  
+					printf("应发工资:%f\t个人所得税:%f\t实发工资:%f\n",S[i].money,S[i].tax,S[i].wages);
+					printf("请输入修改后的数据:\n");
+					printf("\t\t岗位工资:");
+					scanf("%f",&S[i].money1);  
+					printf("\t\t薪级工资:"); 
+					scanf("%f",&S[i].money2);
+					printf("\t\t职务津贴:");  
+					scanf("%f",&S[i].money3);
+					printf("\t\t绩效工资:");  
+					scanf("%f",&S[i].money4);
+					add_money(S);
+					grsds(S);
+					add_wages(S);
+					printf("修改后的数据如下：");
+					printf("工号:%s  %s\n",S[i].id,S[i].name);  
+					printf("岗位工资:%f\t薪级工资:%f\t职务津贴:%f\t绩效工资:%f\n",S[i].money1,S[i].money2,S[i].money3,S[i].money4);  
+					printf("应发工资:%f\t个人所得税:%f\t实发工资:%f",S[i].money,S[i].tax,S[i].wages);
+				}
+				else
+				{
+					printf("没有找到该编号的员工！\n");
+				}
+				printf("\n");
+				system("pause");
+			}
+
+		if(m==2)								//姓名查询
+		{
+			int i;								//现在查询员工序号
+			int flag=0;							//标记员工有没有存在
+			char nm[10];
+			printf("\n");
+			printf("请输入要查询员工的姓名：");
+			scanf("%s",nm);
+			for (i = 0; i < n; i++)
+			{
+				if (strcmp(S[i].name,nm)==0)
+				{
+					flag = 1;
+					break;
+				}
+			}
+			if (flag)
+			{
+				printf("原始数据如下：");
+				printf("工号:%s  %s\n",S[i].id,S[i].name);  
+				printf("岗位工资:%f\t薪级工资:%f\t职务津贴:%f\t绩效工资:%f\n",S[i].money1,S[i].money2,S[i].money3,S[i].money4);  
+				printf("应发工资:%f\t个人所得税:%f\t实发工资:%f\n",S[i].money,S[i].tax,S[i].wages);
+				printf("请输入修改后的数据:\n");
+				printf("\t\t岗位工资:");
+				scanf("%f",&S[i].money1);  
+				printf("\t\t薪级工资:"); 
+				scanf("%f",&S[i].money2);
+				printf("\t\t职务津贴:");  
+				scanf("%f",&S[i].money3);
+				printf("\t\t绩效工资:");  
+				scanf("%f",&S[i].money4);
+				add_money(S);
+				grsds(S);
+				add_wages(S);
+				printf("修改后的数据如下：");
+				printf("工号:%s  %s\n",S[i].id,S[i].name);  
+				printf("岗位工资:%f\t薪级工资:%f\t职务津贴:%f\t绩效工资:%f\n",S[i].money1,S[i].money2,S[i].money3,S[i].money4);  
+				printf("应发工资:%f\t个人所得税:%f\t实发工资:%f",S[i].money,S[i].tax,S[i].wages);
+			}
+			else
+			{
+				printf("没有找到该姓名的员工！\n");
+			}
+			printf("\n");
+			system("pause");
+		}
+
+		if(m==3)							    	//返回
+		{
+			return 0;
+		}
+
+		printf("\t\t是否继续修改?(Y/N)");  
+		scanf("\t\t%c",&flag);  
+	}
+	return 0;  
+}
+
+int write(ZGGZ t[])																											//保存模块
+{
+	int i;
+    FILE *fp;//定义文件指针
+    if ((fp = fopen("gz.dat", "w")) == NULL)
+    {
+        printf("数据保存成功，请检查文件是否存在或损坏后重试!");
+		return 0;
+    }
+	fscanf(fp,"%d",&n);
+	for(i=0;i<n;i++)
+	{
+		fprintf(fp,"总计%d人\n",n);
+		fprintf(fp,"工号:%s  %10s\n",t[i].id,t[i].name);  
+		fprintf(fp,"岗位工资:%f\t薪级工资:%f\t职务津贴:%f\t绩效工资:%f\n",t[i].money1,t[i].money2,t[i].money3,t[i].money4);  
+		fprintf(fp,"应发工资:%f\t个人所得税:%f\t实发工资:%f\n",t[i].money,t[i].tax,t[i].wages);
+	}
+    fclose(fp);
+	printf("\n数据保存成功\n\n");  
+    return 0;
+}
+
+int add(ZGGZ S[])																			//	添加模板
+{		
+	int i;
+	int flag;
+	while(flag!='n'&&flag!='N')
+	{
+		i=n++;
+		system("cls");
+		printf("\n\t\t***********数据添加************\n\n");  
+		printf("请输入职工基本信息:\n");
+		printf("\t\t工号:"); 
+
+		do{																					//防止出现工号相同的情况
+			flag=0;
+			scanf("%s",&S[i].id);
+			for (int j = 0; j < i; j++)
+			{
+				if(strcmp(S[i].id,S[j].id)==0)
+				{
+					printf("工号已存在，请重新输入:");
+					flag=1;
+					break;
+				}
+			}
+		}while(flag==1);
+
+		printf("\t\t姓名:"); 
+			scanf("%s",&S[i].name);
+		printf("\t\t岗位工资:");
+			scanf("%f",&S[i].money1);  
+		printf("\t\t薪级工资:"); 
+			scanf("%f",&S[i].money2);
+		printf("\t\t职务津贴:");  
+			scanf("%f",&S[i].money3);
+		printf("\t\t绩效工资:");  
+			scanf("%f",&S[i].money4);
+
+		add_money(S);
+		grsds(S);
+		add_wages(S);
+
+		printf("\n新增职工数据如下：");
+			printf("工号:%s  %s\n",S[i].id,S[i].name);  
+		printf("岗位工资:%f\t薪级工资:%f\t职务津贴:%f\t绩效工资:%f\n",S[i].money1,S[i].money2,S[i].money3,S[i].money4);  
+			printf("应发工资:%f\t个人所得税:%f\t实发工资:%f\n",S[i].money,S[i].tax,S[i].wages);
+
+		printf("\t\t是否继续添加?(Y/N)");  
+		scanf("\t\t%c",&flag);  
+	}
 	return 0;
 }
 
@@ -292,14 +505,12 @@ int main()
             find(adr);  
             system("pause");  
             break;  
-        case 2:  
-            printf("\n\t\t功能尚未完善，请尽情期待\n");   
-//            modify();  
+        case 2:   
+            modify(adr);  
             system("pause");  
             break;  
-        case 3:  
-            printf("\n\t\t功能尚未完善，请尽情期待\n");  
-//            add()  
+        case 3:   
+            add(adr);  
             system("pause");  
             break;  
         case 4:  
@@ -311,9 +522,8 @@ int main()
             list(adr);  
             system("pause");  
             break;  
-        case 6:  
-            printf("\n\t\t功能尚未完善，请尽情期待\n");  
-//            write();  
+        case 6:   
+            write(adr);  
             system("pause");  
             break;  
 
